161c60399a15e1163c282f37039016ff1cd8399f
ArtiomBuhai <artiom.buhai@example.com>
2024-04-01 23:02:51 +0300

    Add admin controller

---
+++ ArturTI225.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+
+namespace ArturTI225.WebUI.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
