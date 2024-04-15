ceadc8dce074c05086743b904a298adb351fa4bf
ArturTI225 <artur.ti225@example.com>
2024-04-15 21:16:31 +0300

    fix order

--- ArturTI225.Domain/Entities/Order.cs
+++ ArturTI225.Domain/Entities/Order.cs
@@ -1,11 +1,13 @@
 using System;
+using ArturTI225.Domain.Entities.Auth;
 
 namespace ArturTI225.Domain.Entities
 {
     public class Order
     {
         public int Id { get; set; }
-        public int CustomerId { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
         public int CarId { get; set; }
         public Car Car { get; set; }
         public DateTime OrderDate { get; set; }
