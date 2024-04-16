e9171235dafe1e00c3ef14c8ad2834ed03cf47ee
ArtiomBuhai <artiom.buhai@example.com>
2024-04-16 09:26:00 +0300

    add migrations

---
+++ ArturTI225.Infrastructure/Migrations/202404221701489_adminpanel.cs
@@ -0,0 +1,24 @@
+﻿namespace ArturTI225.Infrastructure.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class adminpanel : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Orders", "UserId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Orders", "UserId");
+            AddForeignKey("dbo.Orders", "UserId", "dbo.Users", "UserId", cascadeDelete: true);
+            DropColumn("dbo.Orders", "CustomerId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Orders", "CustomerId", c => c.Int(nullable: false));
+            DropForeignKey("dbo.Orders", "UserId", "dbo.Users");
+            DropIndex("dbo.Orders", new[] { "UserId" });
+            DropColumn("dbo.Orders", "UserId");
+        }
+    }
+}
